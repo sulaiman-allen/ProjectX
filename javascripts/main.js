5165083dc0b20b0676444b08ba21d8169e53c2a5
Sulaiman.Allen <xalerons@gmail.com>
2016-06-15 10:52:57 -0500

    Added testing functionality for firebase retrieval

---
+++ javascripts/main.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var Catalog = angular.module('catalog', ['ngRoute']);
+
+Catalog.config(($routeProvider) => {
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyCSJtE8Eh9Lx1TylzCIg3xmG-HKY6ybxFk",
+    authDomain: "catalogr-52a59.firebaseapp.com",
+    databaseURL: "https://catalogr-52a59.firebaseio.com",
+    storageBucket: "catalogr-52a59.appspot.com",
+  });
+
+  $routeProvider
+  .when('/', {
+    controller: 'MainCtrl',
+    controllerAs: 'main',
+    templateUrl: '/index.html'
+  });
+
+})
+.controller('MainCtrl', function($timeout, TestJson) { 
+	const main = this;
+
+	firebase.database().ref('/').on('value', (snap) => {
+	  const data = snap.val();
+	  console.log("data = ", data);
+	  main.test = data;
+	  console.log("main.test.media = ", main.test.media);
+	  $timeout();
+	});
+
+
+})
+.service('TestJson', function($http) {
+
+	const test = this;
+
+	test.getJson = function(x) {
+		$http.get('testjson/geb.json')
+		.success(function(data) {
+		  x(data);
+		});
+	};
+
+});

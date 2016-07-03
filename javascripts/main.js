9c793416cc2e6473d084195f85203bfee17c6f24
xalerons <xalerons@gmail.com>
2016-07-03 14:03:30 -0500

    Added minor styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,89 +18,111 @@ Catalog.config(($routeProvider) => {
     templateUrl: '/auth/login.html'
   })
   .when('/welcome', {
-  	controller: 'MainCtrl',
-  	controllerAs: 'main',
-  	templateUrl: '/main/welcome.html'
+    controller: 'MainCtrl',
+    controllerAs: 'main',
+    templateUrl: '/main/welcome.html'
   })
   .when('/register', {
-  	controller: 'AuthCtrl',
-  	controllerAs: 'auth',
-  	templateUrl: '/auth/register.html'
+    controller: 'AuthCtrl',
+    controllerAs: 'auth',
+    templateUrl: '/auth/register.html'
   });
 })
-.controller('MainCtrl', function($timeout) { 
-	const main = this;
+.controller('MainCtrl', function($timeout, $uibModal) { 
+  const main = this;
 
-	firebase.database().ref('/').on('value', (snap) => {
-	  const data = snap.val();
-	  // console.log("data = ", data);
-	  main.test = data;
-	  // console.log("main.test.media = ", main.test.media);
-	  $timeout();
-	});
+  firebase.database().ref('/').on('value', (snap) => {
+    const data = snap.val();
+    // console.log("data = ", data);
+    main.test = data;
+    // console.log("main.test.media = ", main.test.media);
+    $timeout();
+  });
 
-	// Onclick event for setting the ownership of an item true or false in firebase
-	main.isOwned = function(id, value) {
+  // Onclick event for setting the ownership of an item true or false in firebase
+  main.isOwned = function(id, value) {
 
-		firebase.database().ref(`/media/${id}`).child("owned").set(value);
-	};
+    firebase.database().ref(`/media/${id}`).child("owned").set(value);
+  };
 
-	// Onclick event for deleting the item from the database
-	main.deleteItem = function(id) {
+  // Onclick event for deleting the item from the database
+  // main.deleteItem = function(id) {
 
-		firebase.database().ref(`/media`).child(id).set(null);
-	};
+  //  firebase.database().ref(`/media`).child(id).set(null);
+  // };
 
-	// Onclick method for setting the type of media for each item
-	main.setType = function(id, value) {
+  main.deleteItem = function (id) {
 
-		firebase.database().ref(`/media/${id}`).child("mediaType").set(value);
-	};
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: '/main/deletemodal.html',
+      // controller: 'MainCtrl',
+      size: 'sm',
+      resolve: {
+        id: function () {
+          return id;
+        }
+      }
+    });
 
-	// Onclick method for editing the current datafield
-	main.ClickToEditCtrl = function(media, field) {
-	  console.log("media = ", media);
-	  media.editorEnabled = !media.editorEnabled;
-	  // media.enableEditor();
-	  // console.log("media.editorEnabled = ", media.editorEnabled);
-	  
-	  media.enableEditor = function() {
-	  	console.log("enableEditor firing");
-	    media.editorEnabled = true;
-	    media.editableTitle = field;
-	  };
-	  
-	  media.disableEditor = function() {
-	  	console.log("disableEditor firing");
-	    media.editorEnabled = false;
-	  };
-	  
-	  media.save = function() {
-	  	console.log("save firing");
-	  	console.log("field = ", field);
-	  	console.log("media.editableTitle = ", media.editableTitle);
-	    field = media.editableTitle;
-	    console.log("field = ", field);
-	    media.disableEditor();
-	  };
+    modalInstance.result.then(function (selectedItem) {
+      console.log(selectedItem);
+      }, function () {
+          console.log('Modal dismissed at: ' + new Date());
+      });
+  };
 
-	  if(media.editorEnabled) {
-	 		console.log("editorEnabled = true");
-	  	media.enableEditor();
-	  } else {
-	 		console.log("editorEnabled = false");
-	  	media.disableEditor();
-	  }
-	};
+
+  // Onclick method for setting the type of media for each item
+  main.setType = function(id, value) {
+
+    firebase.database().ref(`/media/${id}`).child("mediaType").set(value);
+  };
+
+  // Onclick method for editing the current datafield
+  main.ClickToEditCtrl = function(media, field) {
+    console.log("media = ", media);
+    media.editorEnabled = !media.editorEnabled;
+    // media.enableEditor();
+    // console.log("media.editorEnabled = ", media.editorEnabled);
+    
+    media.enableEditor = function() {
+      console.log("enableEditor firing");
+      media.editorEnabled = true;
+      media.editableTitle = field;
+    };
+    
+    media.disableEditor = function() {
+      console.log("disableEditor firing");
+      media.editorEnabled = false;
+    };
+    
+    media.save = function() {
+      console.log("save firing");
+      console.log("field = ", field);
+      console.log("media.editableTitle = ", media.editableTitle);
+      field = media.editableTitle;
+      console.log("field = ", field);
+      media.disableEditor();
+    };
+
+    if(media.editorEnabled) {
+      console.log("editorEnabled = true");
+      media.enableEditor();
+    } else {
+      console.log("editorEnabled = false");
+      media.disableEditor();
+    }
+  };
 });
 // .service('TestJson', function($http) {
 
-// 	const test = this;
+//  const test = this;
 
-// 	test.getJson = function(x) {
-// 		$http.get('testjson/geb.json')
-// 		.success(function(data) {
-// 		  x(data);
-// 		});
-// 	};
+//  test.getJson = function(x) {
+//    $http.get('testjson/geb.json')
+//    .success(function(data) {
+//      x(data);
+//    });
+//  };
 // });
